024afe88abd0e7123129d5df6e3188703bfef581
Mark-Kinyua <markinyua@gmail.com>
2021-11-01 14:17:51 +0300

    Added Circle

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
+void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides );
+
 
 int main( void )
 {
@@ -55,9 +59,12 @@ int main( void )
 
         // render OpenGL here
         glEnableClientState( GL_VERTEX_ARRAY );
-        glVertexPointer( 3, GL_FLOAT, 0, polygonVertices ); // If 4 is entered, what happens??
+        glVertexPointer( 3, GL_FLOAT, 0, polygonVertices ); 
         glDrawArrays( GL_POLYGON, 0, 8 );
         glDisableClientState( GL_VERTEX_ARRAY );
+
+        // render OpenGL here
+        drawCircle( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 120, 36 );
         
         // Swap front and back buffers
         glfwSwapBuffers( window );
@@ -70,4 +77,43 @@ int main( void )
     
     return 0;
 
+}
+
+
+
+
+void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides )
+{
+    GLint numberOfVertices = numberOfSides + 1;
+    
+    GLfloat doublePi = 2.0f * M_PI;
+    
+    GLfloat circleVerticesX[numberOfVertices];
+    GLfloat circleVerticesY[numberOfVertices];
+    GLfloat circleVerticesZ[numberOfVertices];
+    
+    //circleVerticesX[0] = x;
+    //circleVerticesY[0] = y;
+    //circleVerticesZ[0] = z;
+    
+    for ( int i = 0; i < numberOfVertices; i++ )
+    {
+        circleVerticesX[i] = x + ( radius * cos( i * doublePi / numberOfSides ) );
+        circleVerticesY[i] = y + ( radius * sin( i * doublePi / numberOfSides ) );
+        circleVerticesZ[i] = z;
+    }
+    
+    GLfloat allCircleVertices[numberOfVertices * 3];
+    
+    for ( int i = 0; i < numberOfVertices; i++ )
+    {
+        allCircleVertices[i * 3] = circleVerticesX[i];
+        allCircleVertices[( i * 3 ) + 1] = circleVerticesY[i];
+        allCircleVertices[( i * 3 ) + 2] = circleVerticesZ[i];
+    }
+    
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 3, GL_FLOAT, 0, allCircleVertices );
+    glDrawArrays( GL_LINE_STRIP, 0, numberOfVertices );
+    glDisableClientState( GL_VERTEX_ARRAY );
 }
